09593dcd940d305c899b31369bec116489113ff1
srushtiyamanur-alt <srushtiyamanur@gmail.com>
2026-09-11 14:36:37 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,6 +1,9 @@
 age=int(input("Enter your age:"))
 
 if age>=18:
-  print("you are eligible to vote.")
+  print("you are eligible to voter.")
+  print("you can register as a voter.")
 else:
   print("you are not eligible to vote.")
+  print("you must be at least 18 years old.")
+  
